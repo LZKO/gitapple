387d8c18251d9c0168335dad715c8c9970d2babd
liyunyao <1666927605@qq.com>
2019-05-04 08:50:06 +0800

    use git

---
+++ src/test/java/com/example/gitapple/GitTestCase.java
@@ -0,0 +1,15 @@
+package com.example.gitapple; 
+
+/* *
+   * Created by LIYUNYAO on 2019/5/4
+   * Time: 8:46
+   * Year: 2019
+   */
+
+public class GitTestCase {
+
+    private void m1() {
+        System.out.println("11111");
+    }
+
+}
