1154224176385ce472534e014c0f350242cd11ab
liyunyao <1666927605@qq.com>
2019-05-04 08:59:21 +0800

    create file

---
+++ src/test/java/com/example/gitapple/GitUseTest2.java
@@ -0,0 +1,15 @@
+package com.example.gitapple; 
+
+/* *
+   * Created by LIYUNYAO on 2019/5/4
+   * Time: 8:57
+   * Year: 2019
+   */
+
+public class GitUseTest2 {
+
+    private void m1() {
+        System.out.println("111111");
+    }
+
+}
